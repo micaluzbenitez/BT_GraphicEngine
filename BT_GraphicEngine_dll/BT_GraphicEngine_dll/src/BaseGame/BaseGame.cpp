70d7a4a0fb10786cf278322838c8c4b6e13f579a
Mica <benitezmicaelaluz@gmail.com>
2022-08-31 13:09:33 -0300

    Created shader

--- BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/BaseGame/BaseGame.cpp
+++ BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/BaseGame/BaseGame.cpp
@@ -19,6 +19,49 @@ BaseGame::~BaseGame()
     }
 }
 
+/* Create shader */
+static unsigned int CompilerShader(unsigned int type, const string& source)
+{
+    unsigned int id = glCreateShader(GL_VERTEX_SHADER);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        cout << "Failed to compile shader " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << endl;
+        cout << message << endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const string& vertexShader, const string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompilerShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompilerShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs); // Atachea los dos shaders al programa
+    glAttachShader(program, fs);
+    glLinkProgram(program); // Lo linkea
+    glValidateProgram(program); // Valida que este todo bien
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+/* ------------- */
+
 void BaseGame::RunBaseGame()
 {    
     if (Init())
@@ -68,8 +111,34 @@ bool BaseGame::Init()
     _renderer->SetVertexBufferTarget(GL_ARRAY_BUFFER, buffer);
     _renderer->SetVertexBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
+    /* GLEW enabled vertex attrib */
     _renderer->EnableVertexAttribArray(0);
     _renderer->DefineVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    /* Create shader */
+    string vertexShader = 
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n";
+
+    string fragmentShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
+    /* ------------- */
 
     return _window != nullptr;
 }
@@ -82,8 +151,8 @@ void BaseGame::Update()
 
 void BaseGame::Draw()
 {
-    //_renderer->ClearScreen();
-    _renderer->ClearScreenWithColor(1, 0, 0, 1);
+    _renderer->ClearScreen();
+    //_renderer->ClearScreenWithColor(1, 0, 0, 1);
 
     /* GLEW draw mode */
     _renderer->DrawWithoutIndexBuffer(GL_TRIANGLES, 0, 3);
@@ -94,6 +163,7 @@ void BaseGame::Draw()
 
 void BaseGame::Deinit()
 {
+    //glDeleteProgram(shader);
     _window->TerminateLibrary();
 }
  
