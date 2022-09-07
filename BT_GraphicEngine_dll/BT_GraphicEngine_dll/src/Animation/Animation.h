9361cb49c441e1363019dbdd9795d24ef56461c0
Mica <benitezmicaelaluz@gmail.com>
2022-09-07 10:52:55 -0300

    Added animation class

---
+++ BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/Animation/Animation.h
@@ -0,0 +1,17 @@
+#ifndef ANIMATION_H
+#define ANIMATION_H
+
+#include <glfw3.h>
+#include "Export.h"
+using namespace std;
+
+class DllImport Animation
+{
+private:
+
+public:
+	Animation();
+	~Animation();
+};
+
+#endif   
