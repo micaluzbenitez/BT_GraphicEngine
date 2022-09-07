41b4feaceb7168fc48502d7316d447a86f9e7e26
Mica <benitezmicaelaluz@gmail.com>
2022-09-07 11:02:28 -0300

    Added sprite class

---
+++ BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/Sprite/Sprite.h
@@ -0,0 +1,17 @@
+#ifndef SPRITE_H
+#define SPRITE_H
+
+#include <glfw3.h>
+#include "Export.h"
+using namespace std;
+
+class DllImport Sprite
+{
+private:
+
+public:
+	Sprite();
+	~Sprite();
+};
+
+#endif 
