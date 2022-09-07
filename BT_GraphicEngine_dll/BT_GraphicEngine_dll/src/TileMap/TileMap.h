11f6469bb1286efdf1f6b9ef99f0d1caaefde409
Mica <benitezmicaelaluz@gmail.com>
2022-09-07 11:00:36 -0300

    Added tilemap class

---
+++ BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/TileMap/TileMap.h
@@ -0,0 +1,17 @@
+#ifndef TILEMAP_H
+#define TILEMAP_H
+
+#include <glfw3.h>
+#include "Export.h"
+using namespace std;
+
+class DllImport TileMap
+{
+private:
+
+public:
+	TileMap();
+	~TileMap();
+};
+
+#endif 
