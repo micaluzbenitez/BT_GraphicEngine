caffe68210d045fe1bf6ce68ae454ff69f4ac40c
Guido <lpmaleinn@gmail.com>
2022-09-07 10:47:55 -0300

    Updated Material class

--- BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/Material/Material.cpp
+++ BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/Material/Material.cpp
@@ -1,9 +1,95 @@
 #include "Material.h"
+#include <fstream>
+#include <sstream>
 
 Material::Material()
 {
+    ID = NULL;
 }
 
 Material::~Material()
 {
+    glDeleteProgram(ID);
 }
+
+void Material::LoadShaders(const char* vertexPath, const char* fragmentPath)
+{
+    // 1. retrieve the vertex/fragment source code from filePath
+    std::string vertexCode;
+    std::string fragmentCode;
+    std::ifstream vShaderFile;
+    std::ifstream fShaderFile;
+    // ensure ifstream objects can throw exceptions:
+    vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    try
+    {
+        // open files
+        vShaderFile.open(vertexPath);
+        fShaderFile.open(fragmentPath);
+        stringstream vShaderStream;
+        stringstream fShaderStream;
+        // read file's buffer contents into streams
+        vShaderStream << vShaderFile.rdbuf();
+        fShaderStream << fShaderFile.rdbuf();
+        // close file handlers
+        vShaderFile.close();
+        fShaderFile.close();
+        // convert stream into string
+        vertexCode = vShaderStream.str();
+        fragmentCode = fShaderStream.str();
+    }
+    catch (std::ifstream::failure e)
+    {
+        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+    }
+    const char* vShaderCode = vertexCode.c_str();
+    const char* fShaderCode = fragmentCode.c_str();
+
+}
+
+unsigned int Material::CompilerShader(unsigned int type, const string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        cout << "Failed to compile shader " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << endl;
+        cout << message << endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+void Material::CreateMaterial(const string& vertexShader, const string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompilerShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompilerShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs); // Atachea los dos shaders al programa
+    glAttachShader(program, fs);
+    glLinkProgram(program); // Lo linkea
+    glValidateProgram(program); // Valida que este todo bien
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    ID = program;
+}
+
+ void Material::UseMaterial()
+ {
+     glUseProgram(ID);
+ }
--- BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/Material/Material.h
+++ BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/Material/Material.h
@@ -1,17 +1,23 @@
 #ifndef MATERIAL_H
 #define MATERIAL_H
 
+#include <glew.h>
 #include <glfw3.h>
+#include <iostream>
 #include "Export.h"
 using namespace std;
 
 class DllImport Material
 {
 private:
-
+	GLuint ID;
 public:
 	Material();
 	~Material();
+    void LoadShaders(const char* vertexPath, const char* fragmentPath);
+    static unsigned int CompilerShader(unsigned int type, const string& source);
+    void CreateMaterial(const string& vertexShader, const string& fragmentShader);
+    void UseMaterial();
 };
 
 #endif   
