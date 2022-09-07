411d3fbb3a0b529d765a19f19ef4d1d803db2a29
Mica <benitezmicaelaluz@gmail.com>
2022-09-07 10:59:33 -0300

    Added input class

---
+++ BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/Input/Input.h
@@ -0,0 +1,17 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#include <glfw3.h>
+#include "Export.h"
+using namespace std;
+
+class DllImport Input
+{
+private:
+
+public:
+	Input();
+	~Input();
+};
+
+#endif 
