e4d6847fdc18399407732a476847976584636ac0
Mica <benitezmicaelaluz@gmail.com>
2022-09-07 10:57:22 -0300

    Added entity class

---
+++ BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/Entity/Entity.h
@@ -0,0 +1,17 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include <glfw3.h>
+#include "Export.h"
+using namespace std;
+
+class DllImport Entity
+{
+private:
+
+public:
+	Entity();
+	~Entity();
+};
+
+#endif   
