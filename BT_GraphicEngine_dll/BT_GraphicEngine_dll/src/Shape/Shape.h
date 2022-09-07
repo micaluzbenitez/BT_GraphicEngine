65d9bf9af8a776e13e3fb60920a1c523c0a630d9
Mica <benitezmicaelaluz@gmail.com>
2022-09-07 10:41:48 -0300

    Added shape class

---
+++ BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/Shape/Shape.h
@@ -0,0 +1,17 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+
+#include <glfw3.h>
+#include "Export.h"
+using namespace std;
+
+class DllImport Shape
+{
+private:
+
+public:
+	Shape();
+	~Shape();
+};
+
+#endif   
