69874b95ded9c9aecfe945d72ed7e8594dd3ac9c
Mica <benitezmicaelaluz@gmail.com>
2022-09-07 10:58:30 -0300

    Added entity2D class

---
+++ BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/Entity2D/Entity2D.cpp
@@ -0,0 +1,9 @@
+#include "Entity2D.h"
+
+Entity2D::Entity2D()
+{
+}
+
+Entity2D::~Entity2D()
+{
+}
---
+++ BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/Entity2D/Entity2D.h
@@ -0,0 +1,17 @@
+#ifndef ENTITY2D_H
+#define ENTITY2D_H
+
+#include <glfw3.h>
+#include "Export.h"
+using namespace std;
+
+class DllImport Entity2D
+{
+private:
+
+public:
+	Entity2D();
+	~Entity2D();
+};
+
+#endif 
