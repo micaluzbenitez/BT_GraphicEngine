4660f42b8b4e70ef91d28a654541f0d16f604f7f
Mica <benitezmicaelaluz@gmail.com>
2022-09-07 10:56:04 -0300

    Added collision manager class

---
+++ BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/CollisionManager/CollisionManager.h
@@ -0,0 +1,17 @@
+#ifndef COLLISIONMANAGER_H
+#define COLLISIONMANAGER_H
+
+#include <glfw3.h>
+#include "Export.h"
+using namespace std;
+
+class DllImport CollisionManager
+{
+private:
+
+public:
+	CollisionManager();
+	~CollisionManager();
+};
+
+#endif 
