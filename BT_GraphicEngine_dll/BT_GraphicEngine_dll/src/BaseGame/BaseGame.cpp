84cfbd6e0ca947872eb1ce57dd4b2284115f8e93
micalbenitez <benitezmicaelaluz@gmail.com>
2022-09-14 00:02:52 -0300

    Added index buffer to draw a square

--- BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/BaseGame/BaseGame.cpp
+++ BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/BaseGame/BaseGame.cpp
@@ -92,30 +92,45 @@ bool BaseGame::Init()
     _renderer->InitGLEW();
 
     /* GLEW buffer */
-    float positions[6] =
+    float positions[] =
     {
-        -0.5f, -0.5f,
-         0.0f,  0.5f,
-         0.5f, -0.5f
+        -0.5f, -0.5f,  // 0
+         0.5f, -0.5f,  // 1
+         0.5f,  0.5f,  // 2
+        -0.5f,  0.5f   // 3
+    };
+
+    /* Index buffer */
+    unsigned int indices[] =
+    {
+        0, 1, 2,
+        2, 3, 0
     };
     
+    /* Assign out GLEW buffer */
     unsigned int buffer;
     _renderer->AssignVertexBuffer(1, buffer);
     _renderer->SetVertexBufferTarget(GL_ARRAY_BUFFER, buffer);
-    _renderer->SetVertexBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    _renderer->SetVertexBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
     /* GLEW enabled vertex attrib */
     _renderer->EnableVertexAttribArray(0);
     _renderer->DefineVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    /* Create out index buffer object */
+    unsigned int ibo;
+    _renderer->AssignVertexBuffer(1, ibo);
+    _renderer->SetVertexBufferTarget(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    _renderer->SetVertexBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
     
     /* Create shader */
-    ShaderProgramSource source = ParseShader("shaders/Basic.shader");
-    cout << "VERTEX" << endl;
-    cout << source.VertexSource << endl;
-    cout << "FRAGMENT" << endl;
-    cout << source.FragmentSource << endl;
-    _material->CreateMaterial(source.VertexSource, source.FragmentSource);
-    _material->UseMaterial();
+    //ShaderProgramSource source = ParseShader("shaders/Basic.shader");
+    //cout << "VERTEX" << endl;
+    //cout << source.VertexSource << endl;
+    //cout << "FRAGMENT" << endl;
+    //cout << source.FragmentSource << endl;
+    //_material->CreateMaterial(source.VertexSource, source.FragmentSource);
+    //_material->UseMaterial();
     /* ------------- */
 
     return _window != nullptr;
@@ -133,7 +148,8 @@ void BaseGame::Draw()
     //_renderer->ClearScreenWithColor(1, 0, 0, 1);
 
     /* GLEW draw mode */
-    _renderer->DrawWithoutIndexBuffer(GL_TRIANGLES, 0, 3);
+    //_renderer->DrawWithoutIndexBuffer(GL_TRIANGLES, 0, 6); // --> 6 son los vertices que dibujamos
+    _renderer->DrawWithIndexBuffer(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr); // --> 6 son los indices que dibujamos
 
     /* Swap front and back buffers */
     _renderer->SwapBuffers(_window->GetWindow());
