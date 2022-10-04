b5b416e6bd3eefe39651ba06a15d1aa7b1824024
Guido Tello <lpmaleinn@gmail.com>
2022-10-04 16:24:45 -0300

    Updated Entity

--- BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/Entity/Entity.cpp
+++ BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/Entity/Entity.cpp
@@ -2,7 +2,17 @@
 
 Entity::Entity()
 {
-	renderer = nullptr;
+    transform = glm::mat4(1.0f);
+
+    translate = glm::vec3(0.0f);
+    rotation = glm::vec3(0.0f);
+    scale = glm::vec3(1.0f);
+    
+    VBO = 0;
+    VAO = 0;
+    EBO = 0;
+	
+    renderer = nullptr;
 }
 
 Entity::~Entity()
@@ -11,4 +21,34 @@ Entity::~Entity()
         renderer = nullptr;
         delete renderer;
     }
+}
+
+void Entity::SetPosition(float x, float y, float z)
+{
+    translate = glm::vec3(x, y, z);
+}
+
+void Entity::SetRotation(float x, float y, float z)
+{
+    rotation = glm::vec3(x, y, z);
+}
+
+void Entity::SetScale(float x, float y, float z)
+{
+    scale = glm::vec3(x, y, z);
+}
+
+glm::vec3 Entity::GetPosition()
+{
+    return translate;
+}
+
+glm::vec3 Entity::GetRotation()
+{
+    return rotation;
+}
+
+glm::vec3 Entity::GetScale()
+{
+    return scale;
 }
--- BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/Entity/Entity.h
+++ BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/Entity/Entity.h
@@ -11,21 +11,27 @@ class DllApi Entity //Quizas sea abstracta
 {
 protected:
 	Renderer* renderer;
-	/*
-	5 matrices
-		rotacion x
-		rotacion y
-		rotacion z
-		posicion
-		escala
-	*/
+
+	unsigned int VBO; //Vertex Buffer Object
+	unsigned int VAO; //Vertex Array Object
+	unsigned int EBO; //Elements Buffer Object
+	
+	glm::mat4 transform;
+	glm::vec3 translate;
+	glm::vec3 rotation;
+	glm::vec3 scale;
+
 public: 
 	Entity();
 	~Entity();
-	/*
-	setters de todo
-	getters de todo
-	*/
+
+	void SetPosition(float x, float y, float z);
+	void SetRotation(float x, float y, float z);
+	void SetScale(float x, float y, float z);
+
+	glm::vec3 GetPosition();
+	glm::vec3 GetRotation();
+	glm::vec3 GetScale();
 
 	virtual void Draw(GLsizei vertices) = 0;
 	virtual void DrawWithIndexBuffer(GLsizei indices) = 0;
