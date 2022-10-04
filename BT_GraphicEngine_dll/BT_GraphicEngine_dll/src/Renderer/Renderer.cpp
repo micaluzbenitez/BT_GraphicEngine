45cf5e1bd00232d30bd0838aa501bc353b288642
micalbenitez <benitezmicaelaluz@gmail.com>
2022-10-04 19:51:58 -0300

    Deleted unnecesary sizeof

--- BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/Renderer/Renderer.cpp
+++ BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/Renderer/Renderer.cpp
@@ -50,11 +50,11 @@ void Renderer::BindBuffer(GLsizeiptr sizeVertices, GLsizeiptr sizeIndices, const
 
 	// Vertex buffer
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * sizeVertices, vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeVertices, vertices, GL_STATIC_DRAW);
 
 	// Index buffer
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(int) * sizeIndices, indices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeIndices, indices, GL_STATIC_DRAW);
 }
 
 void Renderer::UnBindVertex(unsigned int& VAO, unsigned int& VBO, unsigned int& EBO)
