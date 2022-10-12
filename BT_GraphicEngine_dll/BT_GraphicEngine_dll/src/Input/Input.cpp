ec5ba9f063544ad32bdb59b6612bf2dc6a10ca02
Mica <benitezmicaelaluz@gmail.com>
2022-10-12 12:40:08 -0300

    Added input methods

--- BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/Input/Input.cpp
+++ BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/Input/Input.cpp
@@ -1,9 +1,60 @@
 #include "Input.h"
+#include "Window/Window.h"
 
-Input::Input()
+Input::Input(Window* window)
 {
+	glfwSetKeyCallback(window->GetWindow(), KeyCallback);
+	glfwSetCursorPosCallback(window->GetWindow(), MouseCallback);
+	mousePosition.x = window->GetWidth() / 2;
+	mousePosition.y = window->GetHeight() / 2;
 }
 
 Input::~Input()
 {
+}
+
+bool Input::IsKeyPressed(int keycode, Window* window)
+{
+	int aux = glfwGetKey(window->GetWindow(), keycode);
+	return aux == GLFW_PRESS;
+}
+
+bool Input::IsKeyDown(int keycode, Window* window)
+{
+	list<int>::iterator it = find(keysDown.begin(), keysDown.end(), keycode);
+	if (it != keysDown.end())
+	{
+		keysDown.remove(keycode);
+		return true;
+	}
+	return false;
+}
+
+glm::vec2 Input::GetMousePosition()
+{
+	return mousePosition;
+}
+
+void Input::KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (action == GLFW_PRESS) keysDown.push_front(key);
+	else if(action == GLFW_RELEASE) keysDown.remove(key);
+}
+
+void Input::MouseCallback(GLFWwindow* window, double posX, double posY)
+{
+	if (firstMouse)
+	{
+		mousePosition.x = posX;
+		mousePosition.y = posY;
+		firstMouse = false; 
+	}
+
+	float offsetPosX = posX - mousePosition.x;
+	float offsetPosY = mousePosition.y - posY;   // Coordenadas en Y estan invertidas
+	mousePosition.x = posX;
+	mousePosition.y = posY;
+
+	offsetPosX *= SENSITIVY;
+	offsetPosY *= SENSITIVY;
 }
--- BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/Input/Input.h
+++ BT_GraphicEngine_dll/BT_GraphicEngine_dll/src/Input/Input.h
@@ -3,16 +3,34 @@
 
 #include <glew.h>
 #include <glfw3.h>
+#include <list>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
 #include "Export.h"
 using namespace std;
+
+class Window;
 
 class DllApi Input
 {
 private:
+	list<int> keysDown;
+	glm::vec2 mousePosition;
+	bool firstMouse;
+	const float SENSITIVY = 0.1f;
 
 public:
-	Input();
+	Input(Window* window);
 	~Input();
+
+	bool IsKeyPressed(int keycode, Window* window);
+	bool IsKeyDown(int keycode, Window* window);
+
+	glm::vec2 GetMousePosition();
+
+	void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+	void MouseCallback(GLFWwindow* window, double posX, double posY);
 };
 
 #endif 
